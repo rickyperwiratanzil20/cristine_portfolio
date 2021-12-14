201db6cfc1caff1ac6c7453f2106c85fa5542d67
celestialdust <melisayuliakristin@gmail.com>
2021-12-14 21:44:54 +0100

    Fix a lot of stuff

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const workEl = document.getElementById('projects');
 const aboutEl = document.getElementById('aboutme');
 
 document.getElementById('work').addEventListener('click', () => {
-  workEl.scrollIntoView();
+  workEl.scrollIntoView({ behavior: "smooth" });
 });
 
 document.getElementById('about').addEventListener('click', () => {
-  aboutEl.scrollIntoView();
+  aboutEl.scrollIntoView({ behavior: "smooth" });
 });
